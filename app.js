486d1be6352d7195a93e16e5fec5267ad66cbbf2
Ashish Sangale <aashishsangale07@gmail.com>
2020-03-19 23:00:42 +0530

    adding mongoose

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+
+mongoose.connect("mongodb://localhost/campground");    
+
+var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
 app.set("view engine","ejs");
 
 
-//app.get('/bootstrap.min.js', function(req, res) {
-//res.sendFile(__dirname + '/node_modules/bootstrap/dist/bootstrap.min.js');
+//setting up schema
+var CampgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", CampgroundSchema);
+
+//Campground.create({
+//    name: "Bhandardara lake",
+//    image: "https://images.unsplash.com/photo-1464207687429-7505649dae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+//}, function(err, campground){//
+//    if(err){
+//        console.log(err)
+//    } else {
+//        console.log(campground)
+//    }
+
 //});
 
-var campgrounds = [
-    {name: "Lets Campout", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Pawna Lake", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Bhandardara lake", image: "https://images.unsplash.com/photo-1464207687429-7505649dae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-]
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-    
-    res.render("campgrounds", {campgrounds:campgrounds});
+    // Getting campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
+
+    //res.render("campgrounds", {campgrounds:campgrounds});
 
 });
 
@@ -32,10 +55,16 @@ app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-
-    //redirecting to campgrounds page
-    res.redirect("/campgrounds");
+    
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err)
+        } else {
+            //redirecting to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+    
 });
 
 app.get("/campgrounds/new", function(req,res){
