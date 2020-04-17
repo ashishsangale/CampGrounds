632fe7db340f00ed6eb1c7ef81a8f90ad3efd12e
Ashish Sangale <aashishsangale07@gmail.com>
2020-04-17 22:21:06 +0530

    refactoring app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://localhost/campground");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"))
-seedDB();
+//seedDB();
 
 
 //Passport Config
