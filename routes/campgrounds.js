102bb861c6de1410d1cb5dc02380ccedf3ee2ab8
Ashish Sangale <aashishsangale07@gmail.com>
2020-04-18 13:43:57 +0530

    User-campground association

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,18 +21,24 @@ router.get("/", function(req,res){
 
 });
 
-router.post("/", function(req,res){
+router.post("/",isLoggedIn, function(req,res){
     //getting data from the form 
     var name = req.body.name;
     var image = req.body.image;
-    var description = req.body.description; 
-    var newCampground = {name: name, image: image, description:description}
+    var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    } 
+    var newCampground = {name: name, image: image, description:description, author:author}
+
     
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err)
         } else {
             //redirecting to campgrounds page
+            console.log(newlyCreated)
             res.redirect("/campgrounds");
         }
     });
@@ -40,7 +46,7 @@ router.post("/", function(req,res){
 });
 
 //New Route--> adding new campground
-router.get("/new", function(req,res){
+router.get("/new",isLoggedIn, function(req,res){
     res.render("campgrounds/new.ejs");
 });
 
@@ -57,5 +63,13 @@ router.get("/:id", function(req, res){
     });
     
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login")
+}
 
 module.exports = router;
