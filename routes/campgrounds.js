fd3328fb8a39fbe42c3a771c4689ef524d59e190
Ashish Sangale <aashishsangale07@gmail.com>
2020-04-22 13:36:36 +0530

    Destroy campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -88,6 +88,17 @@ router.put("/:id", function(req, res){
     })
 })
 
+//destroy route
+router.delete("/:id", function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds")
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
