792d305ea59911b59feb6cae94c62d55f49276a7
Ashish Sangale <aashishsangale07@gmail.com>
2020-05-03 12:17:58 +0530

    mongodb uri

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get("/", function(req,res){
     res.render("landing");
 });
  
-app.listen("3000", process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
 });
